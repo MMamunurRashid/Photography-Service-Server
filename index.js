693030b58089b2d9b4b714257184ad88bc2fee34
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-11-09 16:25:05 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mamunlm10.47xczn4.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("Assignment 11 server is running");
 });
