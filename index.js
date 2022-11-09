0258c9dec39470f305731359ca53e03c94de1c81
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-11-09 20:10:41 +0600

    limited data send for home page services card

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
 
@@ -10,7 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mamunlm10.47xczn4.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -18,6 +18,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const packagesCollection = client
+      .db("smile-photography")
+      .collection("packages");
+
+    app.get("/services-limit", async (req, res) => {
+      const query = {};
+      const cursor = packagesCollection.find(query);
+      const packages = await cursor.limit(3).toArray();
+      res.send(packages);
+    });
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = packagesCollection.find(query);
+      const packages = await cursor.toArray();
+      res.send(packages);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Assignment 11 server is running");
